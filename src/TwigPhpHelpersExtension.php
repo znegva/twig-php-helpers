e894673bddc47ca8c0d9063ebbef2a59f5427d36
Martin Kausche <mail@martinkausche.de>
2019-03-12 22:39:26 +0100

    new: Add Extension

---
+++ src/TwigPhpHelpersExtension.php
@@ -0,0 +1,13 @@
+namespace Bolt\Extension\Znegva\TwigPhpHelpers;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * An extension for using PHP functions in twig templates.
+ *
+ * @author Martin Kausche <znegva@users.noreply.github.com>
+ */
+class TwigPhpHelpersExtension extends SimpleExtension
+{
+    // Extension code goes here.
+}
